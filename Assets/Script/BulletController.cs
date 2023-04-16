e32903870c694fa1d82290c05a2a59a12cda9c2c
thiagodalsanto <thiagodalsantodaluz@gmail.com>
2023-04-16 15:48:46 -0300

    Enemies death, bullet as chairs

--- Assets/Script/BulletController.cs
+++ Assets/Script/BulletController.cs
@@ -15,7 +15,7 @@ public class BulletController : MonoBehaviour
             float randomZ = UnityEngine.Random.Range(-20, 20);
 
             var zombie = (GameObject)Instantiate(enemy);
-            zombie.transform.position = new Vector3(randomX, 1, randomZ);
+            zombie.transform.position = new Vector3(randomX, 0, randomZ);
             zombie.transform.rotation = Quaternion.Euler(0, UnityEngine.Random.Range(0, 360), 0);
 
             Destroy(hit);
